3c02990f00a30adbb03fd74ced2df0186f8ab76d
leonelvalentinn <leonelvalentinn@gmail.com>
2024-04-11 12:46:27 -0600

    F

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
 import './App.css'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from 'react-router-dom'
 import { Home } from './pages/Home'
-import { AboutPage } from './pages/AboutPage'
-import { Page404 } from './pages/Page404'
 
 function App () {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='*' element={<Page404 />} />
-      </Routes>
-    </Router>
+    <></>
   )
 }
 
--- src/components/AboutDoctors.jsx
+++ src/components/AboutDoctors.jsx
@@ -8,12 +8,12 @@ export const AboutDoctors = () => {
     <section className='w-full flex justify-center items-center flex-col'>
       <div className='w-full grid grid-cols-1 gap-8 place-content-center max-w-7xl px-4 mb-10'>
         <CardAbout
-          name='Dr. Will Gallegos'
+          name='C.D.E.O. Will Gallegos Gómez'
           imgSrc={Will}
-          imgAlt='Foto de perfil del Dr. Will Gallegos'
+          imgAlt='Foto de perfil del C.D.E.O. Will Gallegos Gómez'
           reverse
         >
-          <li className='mb-2'>Graduado de la Universidad Latinoamericana como cirujano dentista</li>
+          <li className='mb-2'>Licenciatura De Cirujano Dentista. Universidad De Guadalajara. Cucs.</li>
           <li className='mb-2'>Graduado de la Universidad Latinoamericana como cirujano dentista</li>
           <li>Graduado de la Universidad Latinoamericana como cirujano dentista</li>
         </CardAbout>
