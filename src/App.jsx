38e5d4897d0af526551ba825d709e8d304b32d54
leonelvalentinn <leonelvalentinn@gmail.com>
2024-04-11 11:53:05 -0600

    Estoy cansado jefe

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom'
 import { Home } from './pages/Home'
-import { AboutPage } from './pages/AboutPage.jsx'
-import { Page404 } from './pages/Page404.jsx'
+import { AboutPage } from './pages/AboutPage'
+import { Page404 } from './pages/Page404'
 
 function App () {
   return (
-    <Routes>
-      <Route path='/' index element={<Home />} />
-      <Route path='about' element={<AboutPage />} />
-      <Route path='*' element={<Page404 />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='*' element={<Page404 />} />
+      </Routes>
+    </Router>
   )
 }
 
