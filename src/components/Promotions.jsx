688d61a271dd9fe0fa1912a5807fe14cbfb39e32
leonelvalentinn <leonelvalentinn@gmail.com>
2024-04-12 15:53:31 -0600

    Add New Promos

--- src/components/Promotions.jsx
+++ src/components/Promotions.jsx
@@ -1,5 +1,8 @@
 import { CardPromo } from './CardPromo'
-import promo from '../assets/promo.webp'
+import promo from '../assets/Promos1.webp'
+import promo1 from '../assets/Promos2.webp'
+import promo2 from '../assets/Promos3.webp'
+import promo3 from '../assets/Promos4.webp'
 import { TitleSection } from './TitleSection'
 
 // eslint-disable-next-line react/prop-types
@@ -10,11 +13,15 @@ export const Promotions = ({ divRef }) => {
       alt: 'Promoción inicial'
     },
     {
-      imgSrc: promo,
+      imgSrc: promo1,
       alt: 'Promoción inicial'
     },
     {
-      imgSrc: promo,
+      imgSrc: promo2,
+      alt: 'Promoción inicial'
+    },
+    {
+      imgSrc: promo3,
       alt: 'Promoción inicial'
     }
   ]
@@ -23,7 +30,7 @@ export const Promotions = ({ divRef }) => {
     <section className='w-full flex justify-center items-center flex-col mt-14 mb-10' id='promotion' ref={divRef}>
       <div className='w-full flex justify-center items-center flex-col max-w-7xl px-4'>
         <TitleSection title='Promociones' />
-        <div className='w-full mt-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-8'>
+        <div className='w-full mt-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 lg:gap-8'>
           {
             infoPromotions.map((item, i) => (
               <CardPromo
